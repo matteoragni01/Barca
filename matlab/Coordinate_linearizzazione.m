da647dce947677d07fcb0cf5f6717bfada958264
matteoragni01 <matteo.ragni01@universitadipavia.it>
2022-05-17 16:43:45 +0200

    Disaccoppiamento

--- matlab/Coordinate_linearizzazione.m
+++ matlab/Coordinate_linearizzazione.m
@@ -12,12 +12,26 @@ x0 = [0,0,0.3491]';
 
 sys = ss(A,B,C,D);
 
-%% 
+%%  Funzione di trasferimento completa del sistema linearizzato (3x3)
 
-G = tf(sys);
+Gc = tf(sys);
 
-% Pagina 730 libro pdf
-% Pagina 433 libro cartaceo
-%OUT = G * U; 
+%% Funzione di trasferimento con solo gli input da disaccoppiare (2x2)
+
+G(1,1) = Gc(1,1);
+G(1,2) = Gc(1,2);
+G(2,1) = Gc(2,1);
+G(2,2) = Gc(2,2);
+
+%% Funzione di trasferimento in cui gli input sono disaccopiati
+
+Gd(1,1) = G(1,1);
+Gd(1,2) = 0;
+Gd(2,1) = 0;
+Gd(2,2) = G(2,2);
+
+%% DELTA
+
+D= inv(G)*Gd;
 
 
